54686971e6382b84c8b18b9d528e2c5e23594c6b
Aislan Maia <aislan.sousamaia@gmail.com>
2022-02-19 10:28:06 -0300

    Unsure LocalSavePurchases delete old cache upon save

--- src/domain/data/usecases/local-save-purchases.spec.ts
+++ src/domain/data/usecases/local-save-purchases.spec.ts
@@ -1,13 +1,22 @@
 class LocalSavePurchases {
   constructor(private readonly cacheStore: CacheStore) {
   }
+
+  async save(): Promise<void> {
+    this.cacheStore.delete()
+  }
 }
 
 interface CacheStore {
+  delete: () => void
 }
 
 class CacheStoreSpy implements CacheStore{
   deleteCallsCount = 0
+
+  delete(): void {
+    this.deleteCallsCount++
+  }
 }
 
 describe('LocalSavePurchases', () => {
@@ -15,5 +24,11 @@ describe('LocalSavePurchases', () => {
     const cacheStore = new CacheStoreSpy()
     new LocalSavePurchases(cacheStore);
     expect(cacheStore.deleteCallsCount).toBe(0)
+  })
+  test('should delete old cache on save', () => {
+    const cacheStore = new CacheStoreSpy()
+    let localSavePurchases = new LocalSavePurchases(cacheStore);
+    localSavePurchases.save()
+    expect(cacheStore.deleteCallsCount).toBe(1)
   })
 })
